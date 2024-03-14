959ec2fe29504d2c59c13803c34c747ccc78d65a
mattakers20 <128992753+mattakers20@users.noreply.github.com>
2024-03-14 14:17:43 -0400

    Update Cell.java
    added commenting

--- Cell.java
+++ Cell.java
@@ -1,24 +1,27 @@
+/*this class extends upon the JButton 
+class to represent a single cell on the 
+minesweeper board */
+
 package minesweeper;
 
 import javax.swing.*;
 
-
 public class Cell extends JButton{
 	
-	private int rowNum; 
-	private int colNum; 
+	private int rowNum; //represents the row in which the cell is 
+	private int colNum; //represents the column in which the cell is
 	
-	public Cell(int r, int c) {
+	public Cell(int r, int c) { //constructor method
 		super(); 
 		this.rowNum = r; 
 		this.colNum = c; 
 	}
 	
-	public int getRowNum() {
+	public int getRowNum() { //returns the row number
 		return this.rowNum; 
 	}
 	
-	public int getColNum() {
+	public int getColNum() { //returns the column number
 		return this.colNum; 
 	}
 }
